c02d18fd080512e53c6831f523552bb019a01607
Elif Berfin Deniz <elifberfindeniz@hotmail.com>
2024-11-22 15:27:00 +0300

    Replaced skill levels input with Select dropdown

--- src/components/editor/SkillInputGroup.vue
+++ src/components/editor/SkillInputGroup.vue
@@ -1,7 +1,19 @@
 <template>
     <base-input-group>
         <base-input :inputId="nameId" :value="skills[groupIndex]['name']" @update-value="updateSkillValue($event, 'name', groupIndex)">Skill</base-input>
-        <base-input :inputId="levelId" :value="skills[groupIndex]['level']" @update-value="updateSkillValue($event, 'level', groupIndex)">Level</base-input>
+        <div class="level-select">
+            <label :for="levelId" class="form-label text-secondary">Level</label>
+            <select class="form-select bg-secondary-subtle" 
+                    :id="levelId"
+                    v-model="skills[groupIndex]['level']"
+                    @change="updateSkillValue($event.target.value, 'level', groupIndex)">
+                <option value="Novice">Novice</option>
+                <option value="Beginner">Beginner</option>
+                <option value="Intermediate">Intermediate</option>
+                <option value="Proficient">Proficient</option>
+                <option value="Expert">Expert</option>
+            </select>
+        </div>
     </base-input-group>
 </template>
 
@@ -17,3 +29,21 @@
         },
     };
 </script>
+
+<style scope>
+    .level-select{
+        flex: 0 0 calc(50% - 20px);
+        display: flex;
+        flex-direction: column;
+        margin: 0.5rem auto;
+        margin-left: 0;
+    }
+
+    select{
+        height: 3rem;
+    }
+
+    label {
+        font-size: 0.9rem;
+    }
+</style>
